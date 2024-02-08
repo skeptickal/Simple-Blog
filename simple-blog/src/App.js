8f8105653d127a749e8d436a2df17b1a4f659139
Jackson Geer <jacksongeer@gmail.com>
2024-02-08 15:47:24 -0500

    added event and useState functionality

--- simple-blog/src/App.js
+++ simple-blog/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the new blog'
+
   return (
     <div className="App">
-     <div className="content"><h1>App Component</h1></div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
