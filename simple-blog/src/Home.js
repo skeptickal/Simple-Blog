9a54621f814830967c86f9daad2fd67bd3f6d6bf
Jackson Geer <jacksongeer@gmail.com>
2024-02-09 00:51:09 -0500

    messed with HTML tags and anon functions

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -3,18 +3,37 @@ import { useState } from 'react';
 const Home = () => {
     const [name, setName] = useState('mario');
     const [age, setAge] = useState(25);
+    const [inputName, setInputName] = useState('');
+    const [inputAge, setInputAge] = useState('');
+
     const handleClick = (e) => {
-        setName('luigi');
-        setAge(30);
+        setName(inputName);
+        setAge(inputAge);
+    }
+
+    const handleInputNameChange = (e) => {
+        setInputName(e.target.value);
+    }
+
+    const handleInputAgeChange = (e) => {
+        setInputAge(e.target.value);
     }
     
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name} is { age } years old </p>
-            <button onClick={handleClick}>Click Me</button>
+            <p>{name} is {age} years old</p>
+            <label htmlFor="">input name
+            <p></p></label>
+            <input type="text" value={inputName} onChange={handleInputNameChange} placeholder="Enter name"/>
+            <p></p>
+            <label htmlFor="">input name</label>
+            <p><input type="text" value={inputAge} onChange={handleInputAgeChange} placeholder='Enter Age'/></p>
+            
+            <p><button onClick={handleClick}>Click Me</button></p>
+
         </div>
-     );
+    );
 }
- 
+
 export default Home;
