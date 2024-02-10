190e35464693916c2bef439809c48f5b0bafb5ab
Jackson Geer <jacksongeer@gmail.com>
2024-02-09 22:42:56 -0500

    added feaux blog previews

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -1,37 +1,18 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const [inputName, setInputName] = useState('');
-    const [inputAge, setInputAge] = useState('');
-
-    const handleClick = (e) => {
-        setName(inputName);
-        setAge(inputAge);
-    }
-
-    const handleInputNameChange = (e) => {
-        setInputName(e.target.value);
-    }
-
-    const handleInputAgeChange = (e) => {
-        setInputAge(e.target.value);
-    }
-    
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3 },
+    ]);
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <label htmlFor="">input name
-            <p></p></label>
-            <input type="text" value={inputName} onChange={handleInputNameChange} placeholder="Enter name"/>
-            <p></p>
-            <label htmlFor="">input name</label>
-            <p><input type="text" value={inputAge} onChange={handleInputAgeChange} placeholder='Enter Age'/></p>
-            
-            <p><button onClick={handleClick}>Click Me</button></p>
-
+            {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+              <h2>{blog.title}</h2>
+              <p>Written by {blog.author}</p>
+            </div> )) }
         </div>
     );
 }
